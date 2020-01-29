6fc625fb81a41c77fd9d3cab3c28ee81b13362db
James Doe <james@gmail.com>
2020-01-29 09:22:16 +0400

    Compiled assets attached

--- resources/views/frontend/partials/_head.blade.php
+++ resources/views/frontend/partials/_head.blade.php
@@ -3,11 +3,12 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="">
 <title>Edkos - @yield('title', 'Website')</title>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css"
-      integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
 <link rel="icon" href="{{ asset('assets/img/logo.png') }}"/>
-<link rel="stylesheet" href="{{ asset('assets/libs/css/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/libs/css/font-awesome.css') }}">
-<link rel="stylesheet" href="{{ asset('css/font.css') }}">
-<link rel="stylesheet" href="{{ asset('css/main.css') }}">
+{{--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css"--}}
+{{--      integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">--}}
+{{--<link rel="stylesheet" href="{{ asset('assets/libs/css/bootstrap.min.css') }}">--}}
+{{--<link rel="stylesheet" href="{{ asset('assets/libs/css/font-awesome.css') }}">--}}
+{{--<link rel="stylesheet" href="{{ asset('css/font.css') }}">--}}
+{{--<link rel="stylesheet" href="{{ asset('css/main.css') }}">--}}
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 @yield('extra-styles')
--- resources/views/frontend/partials/_scripts.blade.php
+++ resources/views/frontend/partials/_scripts.blade.php
@@ -1,5 +1,6 @@
-<script src="{{ asset('assets/libs/js/jquery-3.3.1.min.js') }}"></script>
-<script src="{{ asset('assets/libs/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/libs/js/popper.min.js') }}"></script>
+{{--<script src="{{ asset('assets/libs/js/jquery-3.3.1.min.js') }}"></script>--}}
+{{--<script src="{{ asset('assets/libs/js/bootstrap.min.js') }}"></script>--}}
+{{--<script src="{{ asset('assets/libs/js/popper.min.js') }}"></script>--}}
+<script src="{{ asset('js/app.js') }}"></script>
 @yield('extra-scripts')
 <script src="{{ asset('js/main.js') }}"></script>
