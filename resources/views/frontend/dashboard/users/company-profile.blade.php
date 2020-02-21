86f0be09cb7ab8fa6d724564bf7704c192e1632c
James Doe <james@gmail.com>
2020-02-21 12:32:55 +0400

    gfdgdfg

--- resources/views/frontend/dashboard/users/company-profile.blade.php
+++ resources/views/frontend/dashboard/users/company-profile.blade.php
@@ -96,7 +96,7 @@
                 </div>
                 <div class="form-all-content">
                     <div class="dashboard-form-block">
-                        <label>Address</label>
+                        <label>Address 777</label>
                         <input type="text" name="street" placeholder="Street Address" value="{{ $user->street }}">
                     </div>
                     <div class="error-message text-danger">
